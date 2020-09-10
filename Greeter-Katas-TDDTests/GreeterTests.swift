190acdf7d37a45ddaf6b09062e23be8cf2121027
Jose <jose@medici.md>
2020-09-10 11:32:00 -0400

    Test drive SUT initialization

---
+++ Greeter-Katas-TDDTests/GreeterTests.swift
@@ -0,0 +1,21 @@
+//
+//  GreeterTests.swift
+//  Greeter-Katas-TDDTests
+//
+//  Created by Roberto Abreu on 9/10/20.
+//  Copyright © 2020 Roberto Abreu. All rights reserved.
+//
+
+import XCTest
+
+struct Greeter {
+    
+}
+
+class GreeterTests: XCTestCase {
+
+    func test_init() {
+        let _ = Greeter()
+    }
+
+}
