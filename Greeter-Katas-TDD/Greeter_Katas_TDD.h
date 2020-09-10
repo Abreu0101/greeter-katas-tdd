9f7dafb9d7c25242b42e99a8d4ebee4f378f7d21
Jose <jose@medici.md>
2020-09-10 11:03:53 -0400

    Initial Commit

---
+++ Greeter-Katas-TDD/Greeter_Katas_TDD.h
@@ -0,0 +1,19 @@
+//
+//  Greeter_Katas_TDD.h
+//  Greeter-Katas-TDD
+//
+//  Created by Roberto Abreu on 9/10/20.
+//  Copyright © 2020 Roberto Abreu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Greeter_Katas_TDD.
+FOUNDATION_EXPORT double Greeter_Katas_TDDVersionNumber;
+
+//! Project version string for Greeter_Katas_TDD.
+FOUNDATION_EXPORT const unsigned char Greeter_Katas_TDDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Greeter_Katas_TDD/PublicHeader.h>
+
+
